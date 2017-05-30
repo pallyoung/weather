3e281142f8eecd3656e3acc3e8331497bdfe438c
Spencer <yeshaopeng@lianqin360.com>
2017-05-30 10:12:35 +0800

    init

---
+++ app/index.js
@@ -0,0 +1,19 @@
+'use strict'
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class Entry extends Component{
+    constructor(...props){
+        super(...props);
+    }
+    render(){
+        return <View>
+            <Text> welcome to wather</Text>
+        </View>
+    }
+}
